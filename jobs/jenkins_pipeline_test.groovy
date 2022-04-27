398d8dcc997edb6053a978126aeabe0429a00494
Cedric Le Coz <cedric.le.coz@accenture.com>
2022-04-27 18:03:24 +0100

    test

--- jobs/jenkins_pipeline_test.groovy
+++ jobs/jenkins_pipeline_test.groovy
@@ -25,7 +25,7 @@ node('ec2-multijob'){
         cleanWs()
         echo "there"
     } 
-    new scripted_stages.CmfScm(this).execute('https://code.rdkcentral.com/r/cmf/manifests', 'cmf-tools.xml')
+    new scripted_stages.CmfScm(this).execute('https://code.rdkcentral.com/r/cmf/manifests', 'cmf-tools.xml', 'rdk-next', '.', 1, '')
 }
 
 //pipeline {
