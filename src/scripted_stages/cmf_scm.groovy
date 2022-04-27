e985f0d056d89c90b07dceeb7ee6d02f1a5d6ff2
Cedric Le Coz <cedric.le.coz@accenture.com>
2022-04-27 17:48:58 +0100

    try to add a scm stage

---
+++ src/scripted_stages/cmf_scm.groovy
@@ -0,0 +1,58 @@
+package scripted_stage
+
+class cmf_scm {
+    private final Script script
+
+    cmf_scm(Script script) {
+        this.script = script
+    }
+    
+    void execute(String url, String manifest='', String branch='rdk-next', String destination='.', Integer depth=0, String credentials='') {
+        script.stage('SCM') {
+            script.echo "Checkout ${url}"
+            if (${manifest} != '' ) {
+                script.echo "Manifest asked, using repo"
+                if (${credentials} != '') {
+                    script.echo "Credentials can't be used with repo"
+                }
+                script.checkout changelog: false, poll: false, \
+                    scm: [$class: 'RepoScm', \
+                        currentBranch: true, \
+                        depth: ${depth}, \
+                        destinationDir: "${destination}", \
+                        jobs: 1, \
+                        manifestBranch: "${branch}", \
+                        manifestFile: "${manifest}", \
+                        manifestRepositoryUrl: "${url}", \
+                        quiet: true, \
+                        resetFirst: true \
+                    ]
+            } else {
+                script.echo "using git"
+                script.checkout changelog: false, poll: false, \
+                    scm: [ \
+                        $class: 'GitSCM', \
+                        branches: [[name: "${branch}"]], \
+                        extensions: [ \
+                            [ \
+                                $class: 'CloneOption', \
+                                depth: ${depth}, \
+                                noTags: true, \
+                                reference: '', \
+                                shallow: true \
+                            ], \
+                            [ \
+                                $class: 'RelativeTargetDirectory', \
+                                relativeTargetDir: "${destination}" \
+                            ] \
+                        ], \
+                        gitTool: 'Default',\
+                        userRemoteConfigs: [[\
+                            credentialsId: "${credentials}",\ 
+                            url: "${url}"\
+                        ]]\
+                    ]
+            }
+        }
+    } 
+}
